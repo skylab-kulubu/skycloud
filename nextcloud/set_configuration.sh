783bfedde7ed199c1355448561375a6d1d4d27ca
Faruk <farukomererdem2003@gmail.com>
2024-11-23 09:14:35 +0000

    config script updated

--- nextcloud/set_configuration.sh
+++ nextcloud/set_configuration.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 docker compose exec -u www-data nc php occ --no-warnings app:install forms
-# adopt to your system e.g. add sudo, www user etc..
-# docker compose exec --user www-data nc php occ app:enable richdocuments
-docker compose exec --user www-data nc php occ config:app:set richdocuments wopi_url --value https://collabora:9980 
-docker compose exec --user www-data nc php occ richdocuments:activate-config
+docker compose exec -u www-data nc php occ --no-warnings app:install richdocuments
+docker compose exec -u www-data nc php occ --no-warnings app:install richdocumentscode
+docker compose exec -u www-data nc php occ --no-warnings app:install contacts
+docker compose exec -u www-data nc php occ --no-warnings app:install mail
+docker compose exec -u www-data nc php occ --no-warnings app:install spreed
 
-# docker compose
-docker compose exec --user www-data nc php occ config:app:set richdocuments wopi_url --value https://collabora:9980 
-docker compose exec --user www-data nc php occ richdocuments:activate-config
+
+
+# docker compose exec --user www-data nc php occ config:app:set richdocuments wopi_url --value https://collabora:9980 
+# docker compose exec --user www-data nc php occ richdocuments:activate-config
+
+# # docker compose
+# docker compose exec --user www-data nc php occ config:app:set richdocuments wopi_url --value https://collabora:9980 
+# docker compose exec --user www-data nc php occ richdocuments:activate-config
 
 # GUI "Administration Settings" > Administration > Office 
 # (https://cloud.mydomain/settings/admin/richdocuments) 
